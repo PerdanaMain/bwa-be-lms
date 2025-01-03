ef981ee2bdeba1f0f26ce68566a4322d791cd3a9
Perdana Main <firman.fp123@gmail.com>
2025-01-03 10:20:54 +0700

    feat: set up verify token middleware

---
+++ src/middlewares/verify-token.middleware.js
@@ -0,0 +1,29 @@
+import jwt from "jsonwebtoken";
+import UserModel from "../models/user.model.js";
+export const verifyToken = async (req, res, next) => {
+  const secretKey = process.env.JWT.SECRET_KEY ?? "";
+
+  //  Authorization: JWT <token>
+  const headerPrefix = req.headers?.authorization?.split(" ")[0];
+  if (headerPrefix === "JWT") {
+    const token = req.headers?.authorization?.split(" ")[1];
+    const decoded = jwt.verify(token, secretKey);
+
+    const user = UserModel.findById(decoded.data.id, "_id name email role");
+
+    if (!user) {
+      return res.status(401).json({ message: "Token expired" });
+    }
+
+    req.user = {
+      _id: user._id.toString(),
+      name: user.name,
+      email: user.email,
+      role: user.role,
+    };
+
+    next();
+  } else {
+    return res.status(400).json({ message: "Unauthorization" });
+  }
+};

8feb0606d69272521bd6b3135fc3ec5c7df75ce7
perdanamain <firman.fp123@gmail.com>
2025-01-16 16:48:31 +0700

    fix: crud course and connect to cloudinary

--- src/controllers/course.controller.js
+++ src/controllers/course.controller.js
@@ -4,6 +4,7 @@ import CategoryModel from "../models/category.model.js";
 import fs from "fs";
 import UserModel from "../models/user.model.js";
 import path from "path";
+import cloudinary from "../utils/cloudinary.js";
 
 export const getCourses = async (req, res) => {
   try {
@@ -20,11 +21,10 @@ export const getCourses = async (req, res) => {
         select: "name",
       });
 
-    const image_url = process.env.APP_URL + "/uploads/courses/";
     const response = courses.map((course) => {
       return {
         ...course.toObject(),
-        thumbnail_url: image_url + course.thumbnail,
+        thumbnailUrl: cloudinary.url(course.thumbnail),
         total_students: course.students.length,
       };
     });
@@ -67,12 +67,16 @@ export const postCourse = async (req, res) => {
       });
     }
 
+    const thumbnailInfo = await cloudinary.uploader.upload(req?.file?.path, {
+      folder: "bwa-lms",
+    });
+
     const course = new CourseModel({
       name: parse.data.name,
       category: category._id,
       description: parse.data.description,
       tagline: parse.data.tagline,
-      thumbnail: req?.file?.filename,
+      thumbnail: thumbnailInfo.public_id,
       manager: req.user._id,
     });
 
@@ -93,6 +97,10 @@ export const postCourse = async (req, res) => {
       },
       { new: true }
     );
+
+    if (req?.file?.path && fs.existsSync(req?.file?.path)) {
+      fs.unlinkSync(req.file.path);
+    }
 
     return res.status(201).json({
       message: "Course created successfully",
@@ -123,38 +131,40 @@ export const updateCourse = async (req, res) => {
     const category = await CategoryModel.findById(parse.data.categoryId);
     const oldCourse = await CourseModel.findById(courseId);
 
-    // delete old thumbnail
-    const dirname = path.resolve();
-    const filePath = path.join(
-      dirname,
-      "public/uploads/courses",
-      oldCourse.thumbnail
-    );
-
-    if (fs.existsSync(filePath)) {
-      fs.unlinkSync(filePath);
-    }
-
     if (!category) {
       return res.status(500).json({
         message: "Category Id not found",
       });
     }
 
-    await CourseModel.findByIdAndUpdate(courseId, {
-      name: parse.data.name,
-      category: category._id,
-      description: parse.data.description,
-      tagline: parse.data.tagline,
-      thumbnail: req?.file ? req.file?.filename : oldCourse.thumbnail,
+    const updateData = {
+      name: req.body.name,
+      category: req.body.categoryId,
+      description: req.body.description,
+      tagline: req.body.tagline,
       manager: req.user._id,
+    };
+
+    if (req?.file?.path && fs.existsSync(req?.file?.path)) {
+      await cloudinary.uploader.destroy(oldCourse.thumbnail);
+
+      const thumbnail = await cloudinary.uploader.upload(req?.file?.path, {
+        folder: "bwa-lms",
+      });
+      updateData.thumbnail = thumbnail.public_id;
+
+      fs.unlinkSync(req.file.path);
+    }
+
+    const course = await CourseModel.findByIdAndUpdate(courseId, updateData, {
+      new: true,
     });
 
     return res.json({
       message: "Update Course Success",
+      data: course,
     });
   } catch (error) {
-    console.log(error);
     return res.status(500).json({
       message: "Internal server error",
       detail: error.message,
@@ -174,18 +184,7 @@ export const deleteCourse = async (req, res) => {
       });
     }
 
-    const dirname = path.resolve();
-
-    const filePath = path.join(
-      dirname,
-      "public/uploads/courses",
-      course.thumbnail
-    );
-
-    if (fs.existsSync(filePath)) {
-      fs.unlinkSync(filePath);
-    }
-
+    await cloudinary.uploader.destroy(course.thumbnail);
     await CourseModel.findByIdAndDelete(id);
 
     return res.json({
